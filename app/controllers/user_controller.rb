0fecac12dfbf5b22e2b50e6aa40726e0663eb12b
PaulineLabaisse <pauline.labaisse@orange.fr>
2018-10-30 20:49:23 +0000

    Add projet ok sans front

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,42 @@
+class UserController < ApplicationController
+	def new
+	end
+
+	def create
+		puts params
+		user = User.new
+		user.username = params["username"]
+		user.email = params["email"]
+		user.bio = params["bio"]
+		user.save
+		puts user
+	end
+
+	def new2
+		@user = User.new
+	end
+
+	def create2
+		puts params
+		user = User.new
+		user.username = params["username"]
+		user.email = params["email"]
+		user.bio = params["bio"]
+		user.save
+		puts user
+	end
+
+	def new3
+		@user = User.new
+	end
+
+	def create3
+		puts params
+		user = User.new
+		user.username = params["username"]
+		user.email = params["email"]
+		user.bio = params["bio"]
+		user.save
+		puts user
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/formulaire', to: 'user#new'
+  post '/formulaire', to: 'user#create'
+
+  get  '/formulaire2', to: 'user#new2'
+  post '/formulaire2', to: 'user#create2'
+
+   get  '/formulaire3', to: 'user#new3'
+  post '/formulaire3', to: 'user#create'
+
 end
